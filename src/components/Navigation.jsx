babd7a65bc788f56688f38b0a147159fe4fb50e1
mahtab900 <mdmahtab9006@gmail.com>
2025-03-06 00:27:15 +0530

    second commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+function Navigation(){
+    return (
+        <>
+        <div className="h-16 bg-green-400 rounded-b-lg flex justify-center items-center ">
+            <a href="#home" className="text-lg font-semibold">TaskFlow</a>
+        </div>
+        </>
+    );
+}
+export default Navigation;
